8f8859faceaab84af53195ff7808e9adf28e61a6
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-08 18:21:21 +0100

    first step solved

--- 2/int_computer.py
+++ 2/int_computer.py
@@ -1,38 +1,30 @@
-from os import path
+
+def parse_sequence(line):
+    return [int(s) for s in line.split(',')]
 
 
-def parse_sequence(string_sequence):
-    return [int(s) for s in string_sequence.split(',')]
+def transform(sequence):
+    i, flag = 0, sequence[0]
 
-
-def transform(int_sequence):
-    i, flag = 0, int_sequence[0]
-
-    while flag != 99 and len(int_sequence) > i:
-        left, right, res = int_sequence[i+1], int_sequence[i+2], int_sequence[i+3]
+    while flag != 99 and len(sequence) > i:
+        left, right, res = sequence[i+1], sequence[i+2], sequence[i+3]
         if flag == 1:
-            int_sequence[res] = int_sequence[left] + int_sequence[right]
+            sequence[res] = sequence[left] + sequence[right]
         else:
-            int_sequence[res] = int_sequence[left] * int_sequence[right]
+            sequence[res] = sequence[left] * sequence[right]
 
         i += 4
-        flag = int_sequence[i]
-
-class IntComputer:
-    def __init__(self, file_name):
-        self.sequence = self._read_nums_from(file_name)
-
-        print("Computer initiated.")
-
-    def _read_nums_from(self, file_name):
-        sequence = []
-        if path.isfile(file_name):
-            with open(file_name, 'r') as infile:
-                for line in infile:
-                    sequence.extend(self.parse_sequence(line))
-
-        return sequence
+        flag = sequence[i]
 
 
 if __name__ == "__main__":
-    IntComputer('input.txt')
+    sequence = []
+    with open('input.txt', 'r') as infile:
+        for line in infile:
+            sequence.extend(parse_sequence(line))
+
+    # apply task magic
+    sequence[1], sequence[2] = 12, 2
+
+    transform(sequence)
+    print(sequence[0])
