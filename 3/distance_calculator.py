8ef2b1130c0e08b2a27dc1d1d6ca1ec179ca396f
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-23 17:25:46 +0100

    tmp

--- 3/distance_calculator.py
+++ 3/distance_calculator.py
@@ -1,6 +1,67 @@
 from shared.parser import Parser
 
+left_map = set()
+closest_intersection = float('inf')
+
+
+def GoRight(num_steps):
+    pass
+
+
+def GoLeft(num_steps):
+    pass
+
+
+def GoUp(num_steps):
+    pass
+
+
+def GoDown(num_steps):
+    pass
+
+
+def StepThroughPath(s):
+    x, y = 0, 0
+    for turn in s.split(','):
+        direction, steps = turn[0], int(turn[1])
+        if direction == 'R':
+            for _ in range(steps):
+                x += 1
+                yield x, y
+        elif direction == 'L':
+            for _ in range(steps):
+                x -= 1
+                yield x, y
+        elif direction == 'U':
+            for _ in range(steps):
+                y += 1
+                yield x, y
+        else:
+            for _ in range(steps):
+                y -= 1
+                yield x, y
+
+
+
+def FindClosestIntersection(s):
+    global closest_intersection
+    for x, y in StepThroughPath(s):
+        if (x, y) in left_map:
+            manhattan_distance = x+y
+            closest_intersection = manhattan_distance if manhattan_distance < closest_intersection else closest_intersection
+
+    return closest_intersection
+
+
+
+def CreateMap(s):
+    for x, y in StepThroughPath(s):
+        left_map.add((x, y))
+
 
 if __name__ == '__main__':
-    for line in Parser.ReadLines('input.txt'):
-        print(line)
+    for line in Parser.ReadLines('test.txt'):  #('input.txt'):
+        if not left_map:
+            CreateMap(line)
+        else:
+            print(FindClosestIntersection(line))
--- 3/distance_calculator_test.py
+++ 3/distance_calculator_test.py
@@ -2,3 +2,5 @@ import unittest
 import distance_calculator
 
 
+class TestDistanceCalculator(unittest.TestCase):
+    def test_
