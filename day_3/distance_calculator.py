07b275488248fe74572b67569a6407919924b4d7
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-23 19:08:25 +0100

    first test case

--- day_3/distance_calculator.py
+++ day_3/distance_calculator.py
@@ -1,10 +1,46 @@
 from shared.parser import Parser
 
 
+def YieldPoints(s):
+    x = y = 0
+    for segment in s.split(','):
+        direction, steps = segment[0], int(segment[1])
+        for _ in range(steps):
+            if direction == 'L':
+                x -= 1
+            elif direction == 'R':
+                x += 1
+            elif direction == 'U':
+                y += 1
+            else:
+                y -= 1
+
+            yield (x, y)
+
+
+def FindClosestIntersection(trail_map, s):
+    closest_dist = None
+    for x, y in YieldPoints(s):
+        if (x, y) in trail_map:
+            distance = abs(x)+abs(y)
+            if closest_dist is None or closest_dist > distance:
+                closest_dist = distance
+
+    return closest_dist
+
+
 def GetClosestIntersection(s1, s2):
-    pass
+    trail = set(point for point in YieldPoints(s1))
+    return FindClosestIntersection(trail, s2)
 
 
 if __name__ == '__main__':
+    first = None
+    second = None
     for line in Parser.ReadLines('input.txt'):
-        pass
+        if first is None:
+            first = line
+        else:
+            second = line
+
+    print(GetClosestIntersection(first, second))
--- day_3/distance_calculator_test.py
+++ day_3/distance_calculator_test.py
@@ -3,9 +3,29 @@ from day_3 import distance_calculator
 
 
 class TestDistanceCalculator(unittest.TestCase):
+    def test_map(self):
+        path = "U1,L2,D3,R4"
+        expected = ((0, 1), (-1, 1), (-2, 1), (-2, 0), (-2, -1), (-2, -2), (-1, -2), (0, -2), (1, -2), (2, -2))
+        res = tuple(point for point in distance_calculator.YieldPoints(path))
+        self.assertEqual(expected, res)
+
+    def test_intersection(self):
+        trail_map = {(0, 1), (-1, 1), (-2, 1), (-2, 0), (-2, -1), (-2, -2), (-1, -2), (0, -2), (1, -2), (2, -2)}
+        path = "L2"
+        expected = 2
+        res = distance_calculator.FindClosestIntersection(trail_map, path)
+        self.assertEqual(expected, res)
+
     def test_case_1(self):
         first = "R8,U5,L5,D3"
         second = "U7,R6,D4,L4"
         expected_dist = 6
+        manhattan_dist = distance_calculator.GetClosestIntersection(first, second)
+        self.assertEqual(expected_dist, manhattan_dist)
+
+    def test_case_2(self):
+        first = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+        second = "U62,R66,U55,R34,D71,R55,D58,R83"
+        expected_dist = 159
         manhattan_dist = distance_calculator.GetClosestIntersection(first, second)
         self.assertEqual(expected_dist, manhattan_dist)
