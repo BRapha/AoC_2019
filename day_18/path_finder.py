ae4b95508e6f3375b7cadf1c2ef7540c2e8f33af
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-26 11:05:03 +0100

    add first test case

--- day_18/path_finder.py
+++ day_18/path_finder.py
@@ -2,8 +2,9 @@ from day_18 import graph_builder
 
 
 def FindShortestPath(graph):
-    start = graph.pop('@')
+    return 0
 
 
 if __name__ == '__main__':
     graph = graph_builder.BuildGraphFromFile('input.txt')
+    print(FindShortestPath(graph))
--- day_18/path_finder_test.py
+++ day_18/path_finder_test.py
@@ -1,5 +1,9 @@
-from day_18 import path_finder
+from day_18 import graph_builder, path_finder
 import unittest
 
 
-# class PathFinderTest(unittest.TestCase):
+class PathFinderTest(unittest.TestCase):
+    def test_case_1(self):
+        graph = graph_builder.BuildGraphFromFile('testcase1.txt')
+        expected_dist = 8
+        self.assertEqual(expected_dist, path_finder.FindShortestPath(graph))
