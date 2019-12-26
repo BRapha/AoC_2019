0a3357e86a012d7e6796e154e8cfac4f327bbb1e
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-26 12:29:30 +0100

    fist test pass

--- day_18/path_finder.py
+++ day_18/path_finder.py
@@ -2,7 +2,32 @@ from day_18 import graph_builder
 
 
 def FindShortestPath(graph):
-    return 0
+    needed_keys = {key for key in graph.keys() if key.islower()}
+    seen = set()
+    cost = 0
+    node = graph['@']
+
+    while node:
+        for key, weight in node.items():
+            if not needed_keys:
+                node = None
+                break
+
+            if key.isupper() and key.lower() in needed_keys:
+                continue
+
+            if key in seen:
+                continue
+
+            if key in needed_keys:
+                needed_keys.remove(key)
+
+            seen.add(key)
+            node = graph[key]
+            cost += weight
+            break
+
+    return cost
 
 
 if __name__ == '__main__':
