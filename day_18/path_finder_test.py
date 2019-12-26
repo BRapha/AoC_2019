cca447d73f97ed7b7f452c47fe032830e6a090ac
Raphael Blistein <raphael.blistein@gmail.com>
2019-12-26 20:04:01 +0100

    refactor

--- day_18/path_finder_test.py
+++ day_18/path_finder_test.py
@@ -7,3 +7,9 @@ class PathFinderTest(unittest.TestCase):
         graph = graph_builder.BuildGraphFromFile('testcase1.txt')
         expected_dist = 8
         self.assertEqual(expected_dist, path_finder.FindShortestPath(graph))
+
+    @unittest.skip("laters")
+    def test_case_2(self):
+        graph = graph_builder.BuildGraphFromFile('testcase2.txt')
+        expected_dist = 86
+        self.assertEqual(expected_dist, path_finder.FindShortestPath(graph))
