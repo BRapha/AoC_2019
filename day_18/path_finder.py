9ece03547eac038e9d0fd5604fa84b97f715dac9
Raphael Blistein <raphael.blistein@gmail.com>
2020-01-17 14:39:31 +0100

    brute forcing shortest path

--- day_18/path_finder.py
+++ day_18/path_finder.py
@@ -1,35 +1,60 @@
 from day_18 import graph_builder
+from copy import deepcopy
 
 
-def FindShortestPath(graph):
-    needed_keys = {key for key in graph.keys() if key.islower()}
-    seen = set()
-    cost = 0
-    node = graph['@']
+def GoAndAbsorbNode(graph, key):
+    new_position = graph.pop(key)
 
-    while node:
-        for key, weight in node.items():
-            if not needed_keys:
-                node = None
-                break
+    # remove all links to absorbed node
+    for k in new_position.keys():
+        del graph[k][key]
 
-            if key.isupper() and key.lower() in needed_keys:
-                continue
+    old_position = graph.pop('@')
+    added_cost = new_position.pop('@')
 
-            if key in seen:
-                continue
+    # Update '@‘ at new position
+    graph['@'] = new_position
+    for k, v in old_position.items():
+        if k not in graph['@']:
+            graph['@'][k] = v + added_cost
 
-            if key in needed_keys:
-                needed_keys.remove(key)
 
-            seen.add(key)
-            node = graph[key]
-            cost += weight
-            break
+    # Update all of '@' neighbors
+    for k, v in graph['@'].items():
+        graph[k]['@'] = v
 
-    return cost
+    return added_cost
+
+
+class PathFinder:
+    def __init__(self, graph):
+        self.cost = None
+        self.graph = graph
+        self.missing_keys = {key for key in graph.keys() if key.islower()}
+
+    def FindShortestPath(self):
+        def ExploreNode(key, cost=0, graph=self.graph, keys=self.missing_keys):
+            if key.isupper() and key.lower() in keys:   # if node is a locked door
+                return
+
+            if key != '@':
+                cost += GoAndAbsorbNode(graph, key)
+
+            # remove key if it exists, because we've now found it
+            keys.discard(key)
+
+            if not keys:
+                self.cost = cost if self.cost is None or cost < self.cost else self.cost
+                return
+
+            for neigh in graph['@'].keys():
+                ExploreNode(neigh, cost, deepcopy(graph), keys.copy())
+
+        ExploreNode('@')
+        return self.cost
 
 
 if __name__ == '__main__':
-    graph = graph_builder.BuildGraphFromFile('input.txt')
-    print(FindShortestPath(graph))
+    graph_from_file = graph_builder.BuildGraphFromFile('input.txt')
+    finder = PathFinder(graph_from_file)
+    print(finder.FindShortestPath())
--- day_18/path_finder_test.py
+++ day_18/path_finder_test.py
@@ -3,13 +3,34 @@ import unittest
 
 
 class PathFinderTest(unittest.TestCase):
+    def test_go_and_absorb(self):
+        graph = graph_builder.BuildGraphFromFile('testcase1.txt')
+        expected = graph_builder.BuildGraph([['#', 'b', '.', 'A', '.', '.', '.', '@', '#']])
+        path_finder.GoAndAbsorbNode(graph, 'a')
+        self.assertEqual(expected, graph)
+
     def test_case_1(self):
         graph = graph_builder.BuildGraphFromFile('testcase1.txt')
         expected_dist = 8
-        self.assertEqual(expected_dist, path_finder.FindShortestPath(graph))
+        finder = path_finder.PathFinder(graph)
+        self.assertEqual(expected_dist, finder.FindShortestPath())
 
-    @unittest.skip("laters")
     def test_case_2(self):
         graph = graph_builder.BuildGraphFromFile('testcase2.txt')
         expected_dist = 86
-        self.assertEqual(expected_dist, path_finder.FindShortestPath(graph))
+        finder = path_finder.PathFinder(graph)
+        self.assertEqual(expected_dist, finder.FindShortestPath())
+
+    def test_case_3(self):
+        graph = graph_builder.BuildGraphFromFile('testcase3.txt')
+        expected_dist = 132
+        finder = path_finder.PathFinder(graph)
+        self.assertEqual(expected_dist, finder.FindShortestPath())
+
+    def test_case_4(self):
+        graph = graph_builder.BuildGraphFromFile('testcase4.txt')
+        expected_dist = 132
+        finder = path_finder.PathFinder(graph)
+        self.assertEqual(expected_dist, finder.FindShortestPath())
+
+
